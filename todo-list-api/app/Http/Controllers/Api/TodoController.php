8bbb4583507fb3009b2e569da1e661f2db5e71da
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-13 15:55:17 +0200

    add index and store function

--- todo-list-api/app/Http/Controllers/Api/TodoController.php
+++ todo-list-api/app/Http/Controllers/Api/TodoController.php
@@ -1,10 +1,28 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        $todoTasks = Todo::all();
+        return $todoTasks;
+    }
+
+    public function store(request $request){
+
+        $title = request()->title;
+        $description = request()->description;
+        //store variables data in database
+        $post = Todo::create([
+            'title' => request()->title,
+            'description' => request()->description
+        ]);
+        return $post;
+    }
 }
--- todo-list-api/routes/api.php
+++ todo-list-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//endpoints
+Route::get('tasks', [TodoController::class, 'index']);
+Route::post('tasks', [TodoController::class, 'store']);
