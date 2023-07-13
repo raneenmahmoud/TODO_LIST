1f4136f2e22330a0b83fa9b2ca229b033e242db9
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-13 17:53:04 +0200

    edit migrations

--- todo-list-api/app/Http/Controllers/Api/TodoController.php
+++ todo-list-api/app/Http/Controllers/Api/TodoController.php
@@ -5,24 +5,54 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use App\Http\Resources\TaskResource;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todoTasks = Todo::all();
-        return $todoTasks;
+        $todoTasks = Todo::all(); //select * from todos
+
+        //to determin the specified columns of each task
+        return TaskResource::collection($todoTasks);
     }
 
-    public function store(request $request){
+    public function store(Request $request){
 
-        $title = request()->title;
-        $description = request()->description;
-        //store variables data in database
-        $post = Todo::create([
-            'title' => request()->title,
-            'description' => request()->description
+        // Validate the request data
+        $validatedTaskData = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
         ]);
-        return $post;
+
+        //store new data in database
+        $task = Todo::create($validatedTaskData);
+
+        // Return the new task as a response
+        return new TaskResource($task);
+    }
+
+    public function update(Request $request, Todo $task)
+    {
+        // Validate the request data
+        $validatedTaskData = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        // Update the attributes of the $task model
+        $task->update($validatedTaskData);
+
+        // Return the updated task as a response
+        return new TaskResource($task);
+    }
+
+    public function destroy(Todo $task)
+    {
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Delete successful'
+        ], 200);
     }
 }
