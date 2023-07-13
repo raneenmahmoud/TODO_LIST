42330f7cf64ef3428b32622b93729b10b94963f1
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-14 01:24:53 +0200

    add todoform component

---
+++ todo-list-app/src/components/TodoForm.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const TodoForm = () => {
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    if (id) {
+      // Fetch task data for editing
+      fetch(`http://localhost:8000/api/tasks/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setTitle(data.data.title);
+          setDescription(data.data.decsription);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [id]);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const todoData = { title, description };
+
+    if (id) {
+      // Update existing task
+      fetch(`http://localhost:8000/api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todoData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setTitle('');
+          setDescription('');
+        })
+        .catch((error) => console.log(error));
+    } else {
+      // Create new task
+      fetch('http://localhost:8000/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todoData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setTitle('');
+          setDescription('');
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="container mt-5 p-3 border">
+      <h2>{id ? 'Edit' : 'Add'} To-Do Task</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label>Title:</label>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Description:</label>
+          <textarea
+            className="form-control"
+            value={description}
+            onChange={handleDescriptionChange}
+            rows="3"
+          />
+        </div>
+        <button type="submit">{id ? 'Update' : 'Save'}</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
