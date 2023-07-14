3381467ef7e9e1bc50b86ebfcd02e5ca08c05ee3
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-14 18:56:40 +0200

    appear name of auth user

--- todo-list-app/src/index.js
+++ todo-list-app/src/index.js
@@ -5,10 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = localStorage.getItem('Name') || 'Guest';
+
+const handleLogout = () => {
+  // Implement your logout logic here
+  // Clear the localStorage or perform any other necessary actions
+  localStorage.clear();
+};
+
 root.render(
-  <React.StrictMode>
+     <React.StrictMode>
+     <div className="container mt-3 d-flex justify-content-end col-3">
+      <select className="form-select">
+        <option value={name}>{name}</option>
+      </select>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
     <App />
   </React.StrictMode>
 );
