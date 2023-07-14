56dc40f972cce03f70ebab4698f399438f1665f0
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-14 19:29:39 +0200

    create Todo seeder

--- todo-list-api/database/seeders/DatabaseSeeder.php
+++ todo-list-api/database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $this->call(
-        //     class: PostSeeder:: class
-        // );
+        $this->call(
+            class: TodoSeeder:: class
+        );
     }
 }
