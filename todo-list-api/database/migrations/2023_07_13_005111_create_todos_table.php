35b31661adaf0b3c03d66e920b514da7fb05226c
Raneen Mahmoud <raneenm260@gmail.com>
2023-07-14 18:50:45 +0200

    edit todos migration

--- todo-list-api/database/migrations/2023_07_13_005111_create_todos_table.php
+++ todo-list-api/database/migrations/2023_07_13_005111_create_todos_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
+            $table->unsignedBigInteger('userid')->nullable();
+            $table->foreign('userid')->references('id')->on('users');
         });
     }
 
